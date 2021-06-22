1b1398da08095b3ed32a4665516b4e28d80cba02
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2021-06-22 18:13:05 -0300

    Aula 2

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,20 @@
-import { useState } from "react"
+import { ButtonHTMLAttributes } from "react"
 
-type ButtonProps = {
-    text?: String;
-}
+
+import '../styles/button.scss';
+
+
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>; 
+
 
 
 // named export 
 export function Button(props: ButtonProps) {
-    const [counter, setCounter] = useState(0);
 
-    function increment(){
-        setCounter(counter+1);
-        
-        
-    }
-
-
-    return(
-        <button onClick={increment}> {counter} </button>
+    return (
+        <button className='button' {...props}/>    
     )
 }
 
 
- 
